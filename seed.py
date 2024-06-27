96ffc6ea970ec2fcbc906395ae5b5c69dc864f40
Shaddy <shadybett540@gmail.com>
2024-06-27 20:09:56 +0300

    done

--- seed.py
+++ seed.py
@@ -2,20 +2,13 @@ from models import User,db,Role,Service, County
 from app import app
 
 with app.app_context():
-    new_user = Service(
-        service_name='salman',
-        provider_id=1,
-    )
-    # db.session.add(new_user)
-    # db.session.commit()
-
     roles = []
     for role_name in ['admin','provider','client']:
         role = Role(role_name=role_name)
         roles.append(role)
 
-        # db.session.add_all(roles)
-        # db.session.commit()
+        db.session.add_all(roles)
+        db.session.commit()
 
     counties = []
     for county_name in [
@@ -70,6 +63,6 @@ with app.app_context():
         county = County(county_name=county_name)
         counties.append(county)
 
-        # db.session.add_all(counties)
-        # db.session.commit()
+        db.session.add_all(counties)
+        db.session.commit()
         
