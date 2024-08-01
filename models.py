14b96a9704c9719c1a7dfe5d16ba84460488f01d
Shaddy <shadybett540@gmail.com>
2024-08-01 10:35:40 +0300

    made tables for messages sent and received in User model

--- models.py
+++ models.py
@@ -1,57 +1,3 @@
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.dialects.postgresql import JSON
-
-# db = SQLAlchemy()
-
-# class User(db.Model):
-#     __tablename__ = 'users'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     first_name = db.Column(db.String(100), nullable=False)
-#     middle_name = db.Column(db.String(100), nullable=True)
-#     last_name = db.Column(db.String(100), nullable=False)
-#     national_id = db.Column(db.BigInteger, nullable=True, unique=True)
-#     phone_number = db.Column(db.BigInteger, nullable=True, unique=True)
-#     email = db.Column(db.String(100), nullable=False, unique=True)
-#     password = db.Column(db.String(255), nullable=False)
-#     image = db.Column(db.String, nullable=True)
-#     photos = db.Column(JSON, nullable=True)
-#     videos = db.Column(JSON, nullable=True)
-#     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
-#     role = db.relationship('Role', backref=db.backref('users', lazy=True))
-#     uuid = db.Column(db.String(36),nullable=True,default='default_uuid_value')
-#     uids = db.Column(db.String(36),nullable=True,default='default_uuid_value')
-#     latitude = db.Column(db.Float, nullable=True)
-#     longitude = db.Column(db.Float, nullable=True)
-#     county = db.Column(db.String,nullable=True)
-#     services = db.relationship('Service', secondary='provider_services', backref=db.backref('providers', lazy=True, cascade="all, delete"))
-#     jobs = db.Column(db.Integer,nullable=True)
-#     likes = db.Column(db.Integer,nullable=True)
-#     unlikes = db.Column(db.Integer,nullable=True)
-
-# class Service(db.Model):
-#     __tablename__ = 'services'
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     service_name = db.Column(db.String(100), nullable=False)
-
-# class ProviderService(db.Model):
-#     __tablename__ = 'provider_services'
-#     service_id = db.Column(db.Integer, db.ForeignKey('services.id', ondelete='CASCADE'), primary_key=True)
-#     provider_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), primary_key=True)
-#     county_id = db.Column(db.Integer, db.ForeignKey('counties.id'))
-
-# class Role(db.Model):
-#     __tablename__ = 'roles'
-#     id = db.Column(db.Integer, primary_key=True)
-#     role_name = db.Column(db.String(100), nullable=False)
-
-# class County(db.Model):
-#     __tablename__ = 'counties'
-#     id = db.Column(db.Integer, primary_key=True)
-#     county_name = db.Column(db.String(100), nullable=False)
-
-
-
-
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import JSON
 
@@ -81,6 +27,9 @@ class User(db.Model):
     jobs = db.Column(db.Integer,nullable=True)
     likes = db.Column(db.Integer,nullable=True)
     unlikes = db.Column(db.Integer,nullable=True)
+    messages_sent = db.relationship('Message', backref='sender',lazy=True)
+    messages_received = db.relationship('Message',backref='receiver', lazy=True)
+
 
 class Photo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
