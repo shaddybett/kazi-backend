a7f9ec0055660d91cd110c97063190476f93a0ba
Shaddy <shadybett540@gmail.com>
2024-08-07 13:53:15 +0300

    changed details model format

--- app.py
+++ app.py
@@ -337,23 +337,7 @@ class Upload(Resource):
             return {'error': 'An error occurred while processing the request'}, 500
 
 class Details (Resource):
-    def post(self):
-        data = request.get_json()
-        senderId = data.get(senderId)
-        user = User.query.filter_by(id=senderId).first()
-        if user:
-            image_url = user.image if user.image else None
-            response = make_response(
-                jsonify({
-                'first_name': user.first_name,
-                'last_name': user.last_name,
-                'image': image_url
-                })
-            )
-            return response
-        else:
-            response = make_response(jsonify({'error':'Error fetching user details'}),404)
-            return response
+    
         
 @app.route('/clean-images', methods=['POST'])
 def clean_images():
