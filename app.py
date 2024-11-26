5c02d25c04ec68aa26b5f86111e931bf4122b88b
Shaddy <shadybett540@gmail.com>
2024-11-26 20:09:03 +0300

    passed decoded url to cloudinary

--- app.py
+++ app.py
@@ -585,8 +585,7 @@ class DeleteUpload(Resource):
             if file_type == 'photo':
                 photo = Photo.query.filter_by(filename=decoded_filename, user_id=user.id).first()
                 if photo:
-                    public_id = os.path.splitext(filename)[0]  # Extract public_id by removing extension
-                    app.logger.info(f"Deleting photo with public_id: {public_id}")
+                    public_id = os.path.splitext(decoded_filename)[0] 
                     cloudinary_response = delete_from_cloudinary(public_id, resource_type="image")
                     if cloudinary_response.get("result") == "ok":
                         db.session.delete(photo)
